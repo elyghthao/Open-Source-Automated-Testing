54921092572afc83263ad5bca660707ce1812fdb
elyghthao <elyghthao@gmail.com>
2024-07-16 22:42:44 -0700

    f

--- src/main/java/DifferentChromeOptions.java
+++ src/main/java/DifferentChromeOptions.java
@@ -17,7 +17,6 @@ public class DifferentChromeOptions {
         options.addArguments("--incognito");//run tests in incognito mode
 
         //gets rid of chrome's password manager popup
-        options.addArguments("--password-store=basic");
         options.addArguments("--guest");
 
         WebDriver driver= new ChromeDriver(options);
